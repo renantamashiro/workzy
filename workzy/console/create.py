3cac0026ee864e72132b81e3687fb3217db58e66
renantamashiro <tamashirorenan@gmail.com>
2020-11-06 01:23:54 -0300

    fix workspaces config file

--- workzy/console/create.py
+++ workzy/console/create.py
@@ -1,4 +1,5 @@
 import click
+import json
 
 from workzy.core.workspace import Workspace
 
@@ -14,24 +15,43 @@ def create(workspace_name: str) -> None:
     workspace = Workspace(workspace_name)
     try:
         while True:
+            basic_usage_help()
             command = input(f"Enter command to run with {workspace.name}: ")
             workspace.append(command)
             print(f"{command} saved! Ctrl+C to close\n")
     except KeyboardInterrupt:
-        print("\n\n")
-    finally:
         if workspace.jobs:
             create_file(workspace)
             print(f"\nTo run it, type `workzy run {workspace.name}`")
         else:
-            print(f"{workspace.name} is not saved> Empty processes list")
+            print(f"{workspace.name} is not saved: Empty processes list")
 
 
 def create_file(workspace):
-    """Deprecated... db it's necessary."""
+    """Create a json file to save workspaces config."""
     try:
-        with open(".jobs", "a") as file:
-            string = ",".join(workspace.jobs)
-            file.write(workspace.name + "," + string + "\n")
-    except IOError as err:
+        with open(".jobs.json", "r") as file:
+            data = json.loads(file.read())
+
+        data[workspace.name] = workspace.jobs
+        data = json.dumps(data)
+
+        with open(".jobs.json", "w") as file:
+            file.write(data)
+    except (IOError, TypeError) as err:
         print(err)
+
+
+def basic_usage_help():
+    help_message = """
+    Open a web page:
+        - just type 'web' follows by the url
+                Example: (web www.google.com)
+
+    Open a folder in the default file manager:
+        - just type 'folder' follows by the directory
+                Example: (folder /home/user/Documents)
+
+    Another commands or programs just enter the name\n
+    """
+    print(help_message)
--- workzy/console/run.py
+++ workzy/console/run.py
@@ -1,20 +1,21 @@
+import os
+import json
+
 import click
 
 from workzy.core.workspace import Workspace
 
 
 @click.command()
-@click.argument("workspace")
-def run(workspace: str) -> None:
+@click.argument("workspace_name")
+def run(workspace_name: str) -> None:
     """Run all commands associated with the workspace.
-    Deprecated... db it's necessary
     """
-    with open("/home/renan/Documents/projects/workzy/.jobs", "r") as file:
+    print(f"Loading {workspace_name}...")
+    with open(os.getcwd() + "/.jobs.json", "r") as file:
         data = file.read()
-    data = data[data.find(workspace):]
-    data = data[: data.find("\n")]
-    data = data.split(",")
-    data.remove(workspace)
-    job = Workspace(workspace)
-    job.jobs = data
-    job.initialize()
+
+    data = json.loads(data)
+    workspace = Workspace(workspace_name)
+    workspace.jobs = data[workspace_name]
+    workspace.initialize()
